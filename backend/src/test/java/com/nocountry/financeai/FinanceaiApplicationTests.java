e770459d10f2d203f3c8648e54ebd5091e038691
Emmanuel Medina <medina.e9@gmail.com>
2026-07-18 13:13:01 -0600

    feat: initialize spring boot backend and setup monorepo structure

---
+++ backend/src/test/java/com/nocountry/financeai/FinanceaiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.nocountry.financeai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Intencionadamente vacío: Este test sirve exclusivamente para verificar
+        // que el contexto de Spring Boot se inicialice correctamente.
+    }
+
+}
